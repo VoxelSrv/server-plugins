e6b0902989a06baab644d34eb662d9aa26e4648f
Patbox <39821509+Patbox@users.noreply.github.com>
2020-08-31 09:31:37 +0200

    Fix

--- welcome.js
+++ welcome.js
@@ -1,19 +1,22 @@
 module.exports = {
 	name: 'Welcome',
 	version: '0.0.2',
-	supports: '>=0.2.0-alpha.4'
-}
+	supported: '>=0.2.0-alpha.4',
+};
 
-const { players, configs } = require('server')
+const { players, configs } = require('server');
 
-const cfg = [...configs]
-configs.save('', 'welcome')
+const load = { text: ['Hello there!'], ...configs.load('', 'welcome') };
 
+let cfg = [];
+if (load.text.length > 0) cfg = load.text;
 
-players.event.on('create', function(player) {
-	setTimeout(function() {
-		cfg.forEach(function(text) { 
-			player.send(data.id, text)
-		})
-	}, 500)
-})
+configs.save('', 'welcome', load);
+
+players.event.on('create', function (player) {
+	setTimeout(function () {
+		cfg.forEach(function (text) {
+			player.send(data.id, text);
+		});
+	}, 500);
+});
